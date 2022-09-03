53ae01b542094b0b1c9dd2926c5312a2c8586889
lichangao1826 <lca1826@163.com>
2022-09-03 17:29:56 +0800

    up

---
+++ example/32-non-blocking-channel-operations/non-blocking-channel-operations.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	// msg 不能被发送到 message 通道，因为这是个无缓冲区通道，
+	// 并且也没有接收者，因此， default 会执行。
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
