b6d60c3295d8b0c1492323736301531991bb02bc
lichangao1826 <lca1826@163.com>
2022-09-04 01:20:55 +0800

    up

---
+++ example/36-tickers/tickers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(500 * time.Microsecond)
+	done := make(chan bool)
+
+	go func() {
+		for true {
+			select {
+			case <-done:
+				fmt.Println("Done")
+				return
+			case t := <-ticker.C:
+				fmt.Println("Tick at", t)
+			}
+		}
+	}()
+
+	time.Sleep(1600 * time.Microsecond)
+	ticker.Stop()
+	done <- true
+	fmt.Println("Ticker stopped")
+}
