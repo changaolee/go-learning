8f39a5975a417cfe3aea10f36f6e2ff09f6d7675
lichangao <changao.li.life@outlook.com>
2022-10-09 17:58:19 +0800

    初始化路由

---
+++ gohub/bootstrap/route.go
@@ -0,0 +1,45 @@
+package bootstrap
+
+import (
+	"github.com/gin-gonic/gin"
+	"gohub/routes"
+	"net/http"
+	"strings"
+)
+
+func SetupRoute(router *gin.Engine) {
+
+	// 注册全局中间件
+	registerGlobalMiddleWare(router)
+
+	// 注册 API 路由
+	routes.RegisterAPIRoutes(router)
+
+	// 配置 404 路由
+	setup404Handler(router)
+}
+
+func registerGlobalMiddleWare(router *gin.Engine) {
+	router.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setup404Handler(router *gin.Engine) {
+	// 处理 404 请求
+	router.NoRoute(func(c *gin.Context) {
+		// 获取 Header 的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+}
--- gohub/main.go
+++ gohub/main.go
@@ -2,42 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
+	"gohub/bootstrap"
 )
 
 func main() {
 	// 初始化 Gin 实例
-	r := gin.New()
-	r.Use(gin.Logger(), gin.Recovery())
+	router := gin.New()
 
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-
-		// 以 JSON 格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
-
-	// 处理 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		// 获取 Header 的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	if err := r.Run(":8000"); err != nil {
-		panic(err)
+	if err := router.Run(":8000"); err != nil {
+		panic(err.Error())
 	}
 }
