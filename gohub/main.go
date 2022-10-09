535eacd8efe3759c8235f7252d1762bb2ac0924a
lichangao <changao.li.life@outlook.com>
2022-10-09 23:46:48 +0800

    配置方案实现（读取空 bug）

--- gohub/main.go
+++ gohub/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
+	fmt.Println("get config", config.Get("app"))
+
 	// 初始化 Gin 实例
 	router := gin.New()
 
@@ -13,7 +30,7 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	if err := router.Run(":8000"); err != nil {
-		panic(err.Error())
+	if err := router.Run(":" + config.Get("app.port")); err != nil {
+		panic(err)
 	}
 }
